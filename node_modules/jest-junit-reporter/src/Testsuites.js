c376a5ea0a89c615ec0eaed6e6b3ada2cc82e0bb
erikmjohnson <erikarisma98@gmail.com>
2019-04-22 18:12:09 -0700

    getting test to work 2

--- node_modules/jest-junit-reporter/src/Testsuites.js
+++
@@ -1,9 +0,0 @@
-const Testsuite = require('./Testsuite');
-
-class Testsuites {
-  constructor (results) {
-    this.testsuites = results.testResults.map((result, i) => new Testsuite(i, result));
-  }
-}
-
-module.exports = Testsuites;
