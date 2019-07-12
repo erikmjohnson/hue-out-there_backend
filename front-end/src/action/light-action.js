bbde82b9cf0246b06a4e838c4aa2d019bbbba122
erikmjohnson <erikarisma98@gmail.com>
2019-07-11 19:20:36 -0700

    setup

--- front-end/src/action/light-action.js
+++
@@ -1,8 +0,0 @@
-import superagent from 'superagent';
-
-const API_URL = 'http://localhost:3000';
-const Turn_On_Lights = '/lightgroup/on';
-
-export const allLightsOn = store => {
-  return superagent.get(`${API_URL}${Turn_On_Lights}`)
-};
--- front-end/src/middleware/thunk-middleware.js
+++
@@ -1,3 +0,0 @@
-export default store => next => action => {
-  (typeof action === 'function' ? action(store) : next(action));
-}
